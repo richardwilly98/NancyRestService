34e6f4281b26e641043553c3ec42340f1b14fd95
Richard Louapre <rlouapre@sbp-mi.com>
2013-03-19 07:41:03 -0400

    Initial commit

---
+++ MainModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NancyRestService
+{
+    public class MainModule : Nancy.NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = _ => { return "Hello REST Service"; };
+        }
+    }
+}
---
+++ MainService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using Nancy.Hosting.Self;
+
+namespace NancyRestService
+{
+    public partial class MainService : ServiceBase
+    {
+        private NancyHost host = null;
+
+        public MainService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            host = new NancyHost(new Uri("http://skipperportal.luckyluke.local:9090"));
+            host.Start();
+        }
+
+        protected override void OnStop()
+        {
+            if (host != null)
+            {
+                host.Stop();
+            }
+        }
+    }
+}
